c3a523c3b63984b8bd1925a2a056440eaa476f85
Pedro Lima <pedrolima.n@gmail.com>
2018-09-18 15:41:40 -0300

    adding tests and travis

---
+++ tests/testthat/tests_arena2r.R
@@ -0,0 +1,31 @@
+library(testthat)
+library(arena2r)
+context("Test tests_arena2r()")
+
+my_path = getwd()
+
+# Generate Plots and Dataframes
+
+results = arena2r::get_simulation_results(path = my_path)
+
+summary = suppressWarnings(arena2r::get_statistics_summary(sim_results = results))
+
+plot_conf = supressWarnings(arena2r::plot_confint(results, "Entity 1.NumberIn"))
+
+plot_scat = supressWarnings(arena2r::plot_scatter(results, "Entity 1.NumberIn", "Entity 1.NumberOut"))
+
+test_that("get_simulation_results retorna data.frame com 4 colunas", {
+  expect_equal(length(names(results)), 4)
+  expect_type(object = results, type = "list")
+})
+
+
+test_that("get_statistics_summary retorna data.frame com 10 colunas", {
+  expect_equal(length(names(summary)), 10)
+  expect_type(object = summary, type = "list")
+})
+
+test_that("plots_sao_gerados", {
+  expect_type(object = plot_conf, type = "list")
+  expect_type(object = plot_scat, type = "list")
+})
